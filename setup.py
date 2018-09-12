62a35f21c6a146f090e29b632adaba03c41d3df5
xrotwang <xrotwang@googlemail.com>
2018-09-12 13:49:16 +0200

    bumped version number

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ from setuptools import setup
 
 setup(
     name='pytest-cldf',
-    version='0.1.1',
+    version='0.2.0',
     description="Easy quality control for CLDF datasets using pytest",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     packages=['pytest_cldf'],
     install_requires=[
-        'pytest>=3.1', 
+        'attrs>=18.2',  # upgrade to the new baseline within our packages.
+        'pytest>=3.1',
         'pycldf>=1.2',
     ],
     entry_points={
         'pytest11': [
-            'pytest_clld = pytest_cldf.plugin',
+            'pytest_cldf = pytest_cldf.plugin',
         ]
     },
     classifiers=[
